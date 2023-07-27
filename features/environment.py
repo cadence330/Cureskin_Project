516c81b159b985afb36affce18b7276855dc7ce0
cadence330 <domonic.davis20@gmail.com>
2023-07-27 10:30:16 -0400

    Added browserstack and it is running fine. Also added the simple logger.

--- features/environment.py
+++ features/environment.py
@@ -5,15 +5,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 from app.application import Application
 from selenium.webdriver.chrome.options import Options
 from support.logger import logger
+import allure
+from allure_commons.types import AttachmentType
 
 
 # Allure command:
 # python3 -m behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/main_page_tests.feature
 
 
-def browser_init(context):
+def browser_init(context, test_name):
     """
     :param context: Behave context
+    :param test_name: scenario.name
     """
     service = Service(executable_path=r'C:\Users\domon\OneDrive\Desktop\Cureskin Project\chromedriver.exe')
 
@@ -30,16 +33,16 @@ def browser_init(context):
     context.driver.maximize_window()
 
     #### BROWSERSTACK ####
-    # desired_cap = {
-    #     'browser': 'Firefox',
-    #     'os_version': '11',
-    #     'os': 'Windows',
-    #     'name': test_name
-    # }
-    # bs_user = 'domonicdavis_fPl95A'
-    # bs_key = 'yBJZyz2qsiTg7xotNiuh'
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    desired_cap = {
+        'browser': 'Chrome',
+        'os_version': '11',
+        'os': 'Windows',
+        'name': test_name
+    }
+    bs_user = 'domonicdavis_fPl95A'
+    bs_key = 'yBJZyz2qsiTg7xotNiuh'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     context.driver.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
@@ -48,17 +51,33 @@ def browser_init(context):
 
 
 def before_scenario(context, scenario):
-    print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    # print('\nStarted scenario: ', scenario.name)
+    logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
-    print('\nStarted step: ', step)
+    # print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
 
 
 def after_step(context, step):
     if step.status == 'failed':
+        logger.error(f'Step failed: {step}')
         print('\nStep failed: ', step)
+        # Mark test case as FAILED on BrowserStack:
+        # Documentation: https://www.browserstack.com/docs/automate/selenium/view-test-results/mark-tests-as-pass-fail
+        # context.driver.execute_script(
+        #     'browserstack_executor: {"action": "setSessionStatus", "arguments": '
+        #     '{"status":"failed", "reason": "Step failed"}}'
+        # )
+
+        # Attach a screenshot to Allure report in case the step fails:
+        # allure.attach(
+        #     context.driver.get_screenshot_as_png(),
+        #     name=f'{step.name}.png',
+        #     attachment_type=AttachmentType.PNG
+        # )
 
 
 def after_scenario(context, feature):
